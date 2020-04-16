47c8f488961fc7635e95790275455db44c623633
Tri Nguyen <tringuyenminh23@gmail.com>
2020-04-16 13:39:26 +0300

    improved test infra

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ A skeleton for a generic python project
 
 ### [Test infrastructure](tests)
 Based on [unittest](https://docs.python.org/3/library/unittest.html):
-- Customized [TestCase](tests/utils/test_case.py) with timer, set up io folder and file compare support
-- Customized [TestResult](tests/utils/test_result.py) that log metrics and interactive file compare with meld
-- Customized [CLI](tests/test.py):
+- Customized [TestCase](tests/cases.py) with timer, set up io folder and file comparison support
+- Customized [TestResult](tests/results.py) that log metrics and interactive file compare with meld
+- Customized [Test CLI](tests/parser.py):
     - Discover suite, case, method based on hierarchy
     - Flag option for test dir, metric file, verbosity, and meld  
--- utils/generic_utils.py
+++ utils/generic_utils.py
@@ -1,18 +1,25 @@
-from pathlib import Path
 import logging
+import time
+from pathlib import Path
 
 
 def project_root_path() -> Path:
     return Path(__file__).parent.parent
 
 
-def set_up_logger(log_file_path : Path = None, default_logging_level=logging.INFO):
-    if log_file_path:
-        log_file_path = str(log_file_path)
-    logging.basicConfig(filename=log_file_path, level=default_logging_level,
-                        format='%(asctime)-5s %(name)-5s %(levelname)-10s %(message)s',
-                        datefmt='%H:%M:%S')
+def set_up_logger(log_file_path : Path = None, logging_level: int = logging.INFO):
     logging.VERBOSE = 5
     logging.addLevelName(logging.VERBOSE, "VERBOSE")
     logging.Logger.verbose = lambda inst, msg, *args, **kwargs: inst.log(logging.VERBOSE, msg, *args, **kwargs)
     logging.verbose = lambda msg, *args, **kwargs: logging.log(logging.VERBOSE, msg, *args, **kwargs)
+
+    if log_file_path:
+        if not log_file_path.parent.exists():
+            raise ValueError(f"Log file directory {log_file_path.parent} does not exist")
+    logging.basicConfig(
+        filename=log_file_path,
+        level=logging_level,
+        format='%(asctime)s %(name)-20s %(levelname)-10s %(message)s',
+        datefmt="%Y-%m-%dT%H:%M:%S%z"
+    )
+    logging.Formatter.converter = time.gmtime
