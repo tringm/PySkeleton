ddae32e006d22a51754968de42fa3a9defc2b5ee
Tri Nguyen <tringuyenminh23@gmail.com>
2020-07-15 14:56:39 +0300

    improve logging utils

--- utils/generic_utils.py
+++ utils/generic_utils.py
@@ -1,25 +1,5 @@
-import logging
-import time
 from pathlib import Path
 
 
 def project_root_path() -> Path:
     return Path(__file__).parent.parent
-
-
-def set_up_logger(log_file_path : Path = None, logging_level: int = logging.INFO):
-    logging.VERBOSE = 5
-    logging.addLevelName(logging.VERBOSE, "VERBOSE")
-    logging.Logger.verbose = lambda inst, msg, *args, **kwargs: inst.log(logging.VERBOSE, msg, *args, **kwargs)
-    logging.verbose = lambda msg, *args, **kwargs: logging.log(logging.VERBOSE, msg, *args, **kwargs)
-
-    if log_file_path:
-        if not log_file_path.parent.exists():
-            raise ValueError(f"Log file directory {log_file_path.parent} does not exist")
-    logging.basicConfig(
-        filename=log_file_path,
-        level=logging_level,
-        format='%(asctime)s %(name)-20s %(levelname)-10s %(message)s',
-        datefmt="%Y-%m-%dT%H:%M:%S%z"
-    )
-    logging.Formatter.converter = time.gmtime
